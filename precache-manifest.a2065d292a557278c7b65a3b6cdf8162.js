dd2a2a6c6738cc9826d1a755ab1979a79517566b
jidemobell <jidd32@gmail.com>
2020-02-13 19:11:34 +0100

    Updates

---
+++ precache-manifest.a2065d292a557278c7b65a3b6cdf8162.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232bb8491b9f165b3e8fe91128d722e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e93115bb88d7eb20cda",
+    "url": "/static/css/main.f87fd3c3.chunk.css"
+  },
+  {
+    "revision": "e3578f17e4e246d4e90c",
+    "url": "/static/js/2.a1d1326f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.a1d1326f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e93115bb88d7eb20cda",
+    "url": "/static/js/main.9dfc548a.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "a62b69e5373fa2f72533755927ebd5bd",
+    "url": "/static/media/big_sign.a62b69e5.png"
+  },
+  {
+    "revision": "4b48e739204e22c87e3e503b80c50373",
+    "url": "/static/media/fb.4b48e739.svg"
+  },
+  {
+    "revision": "2295a344ff830939116d39e31a0a2a44",
+    "url": "/static/media/ig2.2295a344.svg"
+  },
+  {
+    "revision": "b7d50938641b026e0cb57bee345a32b5",
+    "url": "/static/media/wedding-2.b7d50938.jpg"
+  },
+  {
+    "revision": "0cb70df9b4f6b1c1baa466bab861dbc7",
+    "url": "/static/media/wedding-3.0cb70df9.jpg"
+  },
+  {
+    "revision": "cbcebcfedf1ea7b2292aafae040c23b1",
+    "url": "/static/media/wedding-4.cbcebcfe.jpg"
+  },
+  {
+    "revision": "af186ec9ecebf9f3bf1cd9ccc0c4bf3a",
+    "url": "/static/media/wedding-5.af186ec9.jpg"
+  },
+  {
+    "revision": "a494d239959c485830bb648e241d979e",
+    "url": "/static/media/wedding-6.a494d239.jpg"
+  },
+  {
+    "revision": "44e858bd55af5e992da632c9e8d45e9f",
+    "url": "/static/media/woman-4.44e858bd.jpg"
+  },
+  {
+    "revision": "60c10f246127409e8907c2e79d0d16e0",
+    "url": "/static/media/woman.60c10f24.jpg"
+  }
+]);
