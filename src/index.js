39a6fae9ce293569a853074ae86258b9d550e19f
jidemobell <jidd32@gmail.com>
2020-02-15 01:05:37 +0100

    update image upload for heroku

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ const logger = tracer.colorConsole({
 const app = express()
 app.use(cors)
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 console.log(process.env.PORT)
 
 app.get('/', (req, res) => {
